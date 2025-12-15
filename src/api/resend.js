0f7b26cc1122ed04963e3c80e65de53905c3c435
Borenp <borenkpardo@gmail.com>
2025-12-15 17:26:20 -0500

    permitir options

--- src/api/resend.js
+++ src/api/resend.js
@@ -3,6 +3,10 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.env.VITE_RESEND_KEY);
 
 export default async function handler (req, res){
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+
     if (req.method !== 'POST') {
         return res.status(405).end();
     }
